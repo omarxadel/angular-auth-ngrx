4c1d5a76e654d27e1b3a916d93d810533046b383
Omar Adel <omarxadel21@gmai.com>
2021-10-05 13:17:26 +0200

    fix: uncomplete route imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/signup/signup.component';
+import { LogInComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    SignupComponent,
-    LoginComponent
+    SignUpComponent,
+    LogInComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
